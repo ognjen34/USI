14e3ff7fbe39a13983b1771e6ff8680829c75e10
Ognjen Radovanovic <ognjenkg34@gmail.com>
2022-03-25 18:48:38 +0100

    promene lool xd

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// Hello World! program
+namespace HelloWorld
+{
+    class Program {         
+        static void Main(string[] args)
+        {
+            while(true) {
+
+            string Unos = Console.ReadLine();
+            if(Unos == "x") {break;}
+            string[] tokens = Unos.Split(" ");
+            int priv = Int32.Parse(tokens[0]);
+            int drugi = Int32.Parse(tokens[2]);
+            string znak = tokens[1];
+
+            if (znak == "+") {
+                Console.WriteLine(priv + drugi);
+            }
+            
+
+            }
+
+            }
+        }
+    }
